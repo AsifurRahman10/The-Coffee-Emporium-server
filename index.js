02d7030c8d7ec75f13964983e94cd739b1b34f9f
AsifurRahman10 <1720105@iub.edu.bd>
2024-12-02 15:41:57 +0600

    server ready

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ async function run() {
 
         const coffeeCollection = client.db('coffeeDB').collection('coffee');
 
+        const userCollection = client.db('coffeeUserDB').collection('user');
+
         // get
 
         app.get('/coffee', async (req, res) => {
@@ -42,11 +44,43 @@ async function run() {
             const result = await coffeeCollection.findOne(query);
             res.send(result);
         })
+
+        // get all user data 
+        app.get('/users', async (req, res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+
+
         // post 
 
         app.post('/coffee', async (req, res) => {
             const coffeeData = req.body;
             const result = await coffeeCollection.insertOne(coffeeData);
+            res.send(result);
+        })
+
+        // user post operation 
+        app.post('/user', async (req, res) => {
+            const userData = req.body;
+            console.log(userData);
+            const result = await userCollection.insertOne(userData);
+            res.send(result);
+        })
+
+        // user last login update api
+
+        app.patch('/users', async (req, res) => {
+            const id = req.body.email;
+            const filter = { email: id };
+            const updateUser = {
+                $set: {
+                    lastSignInTime: req.body?.lastCreated,
+                }
+            }
+            const result = await userCollection.updateOne(filter, updateUser);
             res.send(result);
         })
 
@@ -76,6 +110,14 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await coffeeCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // user delete 
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
             res.send(result);
         })
 
